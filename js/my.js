296e8b89d087223fc8942ff2a65645f20c4a19ba
bhavyaPH <bhavya.dev@gmail.com>
2013-01-06 10:46:46 +0800

    youtube repeater
    submit the a youtube link which you want to repeat some number of times.
    ( finite other repeaters online are repeating videos infinte number of
    times)

---
+++ js/my.js
@@ -0,0 +1,103 @@
+		 $(document).ready(function() {
+		 	//$('#menu1').load('home.html')
+$('#spinner').hide()
+		 	$("li").click(function(){
+		  // If this isn't already active
+		  if (!$(this).hasClass("active")) {
+		    // Remove the class from anything that is active
+		    $("li.active").removeClass("active");
+		    // And make this active
+		    $(this).addClass("active");
+		}
+	});
+		 	
+
+
+		 	$('ul li a').live('click', function() {
+                $('#spinner').show()
+		 		var clickid = $(this).parent('li').index();
+		 		var myid = $(this).attr('id');
+		 		
+		 		switch (myid)
+		 		{
+		 			case "home":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+	                //$('#slider').css('display','block');
+		 			break;
+		 			case "aboutus":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			
+		 			$('#menu1').load('aboutus.html',function() {
+		 				$('#spinner').hide()})
+		 			
+		 			break;
+		 			case "progs":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('programs.html',function() {
+		 				$('#spinner').hide()});
+		 			
+		 			break;
+		 			case "jpnagar":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('jpnagar.html',function() {
+		 				$('#spinner').hide()})
+		 			break;
+		 			case "indranagar":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('indranagar.html',function() {
+		 				$('#spinner').hide()})
+		 			break;
+		 			case "testimonial":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('testimonial.html',function() {
+		 				$('#spinner').hide()})
+		 			break;
+		 			case "calendar":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('calendar.html')
+		 			break;
+		 			case "gallery":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('gallery.html');
+		 			$( "#myCarousel" ).carousel();
+		 			//alert(myid);
+		 			break;
+		 			case "admissions":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('admission.html')
+		 			break;
+		 			case "foodmenu":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('foodmenu.html')
+		 			break;
+		 			case "careers":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('careers.html')
+		 			break;
+		 			case "contact":
+		 			$('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('contactus.html')
+		 			break;
+                    case "feedback":
+                    $('#menu1').html(" ");
+		 		$('#slider').css('display','none');
+		 			$('#menu1').load('feedback.html')
+		 			break;
+		 		};
+	//alert(myid);
+		 		//alert(clickid);
+
+		 	});
+		});
